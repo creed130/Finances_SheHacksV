3338dfc8508d36e2f9832d3fc0d7de25f5e0ac12
Jen Kovinich <jenkovinich@gmail.com>
2021-01-09 11:47:42 -0500

    modifications

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import tkinter as tk
 
-def print_hi(name):
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Hi', name)
-
 
 if __name__ == '__main__':
-    entry = input("What is your name? ")
-    print_hi(entry)
 
-    window = tk.Tk()
-    greeting = tk.Label(text="Hello")
+    window = tk.Tk(None, None, 'adulting', 1)
 
-    greeting.pack
+    greeting = tk.Label( text="Hello")
+
+    greeting.pack()
 
     window.mainloop()
