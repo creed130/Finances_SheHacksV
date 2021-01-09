4314d1ee192dd2a66124e35c8cdae448c3df4ff6
C Reed <64921881+creed130@users.noreply.github.com>
2021-01-09 15:21:39 -0500

    main just calls

---
+++ gameMenu.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+class gameMenu(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+
+    def create_widgets(self):
+        self.button = tk.Button(self,
+            text="Click here to begin!",
+            width=25,
+            height=5,
+            bg="white",
+            fg="red",
+            command=self.sayHi
+        )
+
+
+        self.entry = tk.Entry()
+
+        self.bquit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+
+
+        self.hiLabel = tk.Label(
+            text="Hello Tkinter",
+            fg="white",
+            bg="black",
+            width=10,
+            height=4
+        )
+
+        self.button.pack(side="top")
+        self.entry.pack(side="bottom")
+        self.hiLabel.pack(side="bottom")
+        self.bquit.pack(side="bottom")
+
+    def sayHi(self):
+        # print out hello and their name
+        name = self.entry.get()
+        print("Hello", name)
+        self.entry.delete(0, tk.END)  # empty the text box
+
