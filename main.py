e842b4ac307cdc3ed2aa0b960cdc0f1d47cfa3f7
Al Wong <wong.alison686@gmail.com>
2021-01-09 14:46:53 -0500

    Removed experimental code

--- main.py
+++ main.py
@@ -15,34 +15,34 @@ class Game(tk.Frame):
             height=5,
             bg="white",
             fg="red",
-            command=self.sayHi
+            #command=self.sayHi
         )
 
 
-        self.entry = tk.Entry()
+        #self.entry = tk.Entry()
 
         self.bquit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
 
 
-        self.hiLabel = tk.Label(
-            text="Hello Tkinter",
-            fg="white",
-            bg="black",
-            width=10,
-            height=4
-        )
+        #self.hiLabel = tk.Label(
+            #text="Hello Tkinter",
+            #fg="white",
+            #bg="black",
+            #width=10,
+            #height=4
+        #)
 
         self.button.pack(side="top")
-        self.entry.pack(side="bottom")
-        self.hiLabel.pack(side="bottom")
+        #self.entry.pack(side="bottom")
+        #self.hiLabel.pack(side="bottom")
         self.bquit.pack(side="bottom")
 
-    def sayHi(self):
+    #def sayHi(self):
         # print out hello and their name
-        name = self.entry.get()
-        print("Hello", name)
-        self.entry.delete(0, tk.END)  # empty the text box
+        #name = self.entry.get()
+        #print("Hello", name)
+        #self.entry.delete(0, tk.END)  # empty the text box
 
 root = tk.Tk()
 app = Game(master=root)
