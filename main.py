1ad1fdd98bf02dcd59534640b54ad330d45455aa
C Reed <64921881+creed130@users.noreply.github.com>
2021-01-08 23:10:27 -0500

    a GUI opens

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 
 def print_hi(name):
     # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -7,3 +8,10 @@ def print_hi(name):
 if __name__ == '__main__':
     entry = input("What is your name? ")
     print_hi(entry)
+
+    window = tk.Tk()
+    greeting = tk.Label(text="Hello")
+
+    greeting.pack
+
+    window.mainloop()
