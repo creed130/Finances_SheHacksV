b47b8888e86663a293adb3bbfb873f5b8eb37f87
Charlotte Reed <charlotte.reed@bnc.ox.ac.uk>
2024-10-29 14:48:23 +0000

    Make it work and open the menus

--- gameMenu.py
+++ gameMenu.py
@@ -2,13 +2,12 @@ import tkinter as tk
 import gamePlay
 
 
+# class gameMenu(tk.Frame):
+#     def __init__(self, HOUSE='cheap', master=None):
+#         self=self
+
 class gameMenu(tk.Frame):
     def __init__(self, HOUSE='cheap', master=None):
-
-
-class GameMenu(tk.Frame):
-    def __init__(self, master=None):
-
         super().__init__(master)
         self.master = master
         self.pack()
